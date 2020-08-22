1f2c1684210681383e629e4e209baea448b10719
Nastya Keller <nastykeller@gmail.com>
2020-08-22 16:33:33 +0400

    initial

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,34 @@
 module.exports = {
   root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
   parserOptions: {
     parser: 'babel-eslint'
   },
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  extends: [
+    'plugin:vue/recommended',
+    'eslint:recommended',
+    'plugin:vue/essential'
+  ],
+  plugins: ['vue'],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'generator-star-spacing': 'off',
+    'space-before-function-paren': ['error', 'always'],
+    'space-before-blocks': ['error', 'always'],
+    'vue/max-attributes-per-line': ['error', {
+        singleline: 1,
+        multiline: {
+          max: 1,
+          allowFirstLine: false
+        }
+      }
+    ],
+    'vue/require-default-prop': 'off',
+    'vue/attribute-hyphenation': 'off'
   }
 }
